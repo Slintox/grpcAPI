814a2d2b3f1e47050518e06ea90f7982b22a44d5
Alexey Sakharov <a.saharov@ficto.ru>
2022-11-25 16:51:31 +0300

    work with errors

--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -22,7 +22,39 @@ func NewCalculatorGrpcHandler(c contract.Calculator) *GrpcHandler {
 	}
 }
 
-func (h *GrpcHandler) Add(ctx context.Context, req *api.CalculatorRequest) (*api.CalculatorResponse, error) {
+func (h *GrpcHandler) Add(ctx context.Context, req *api.CalculatorRequest) (*api.CalculatorResponseWithStatus, error) {
+	var pm1, pm2 float64
+	var err error
+	pm1, err = strconv.ParseFloat(req.Parameter1, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	pm2, err = strconv.ParseFloat(req.Parameter1, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	reqM := &models.AddRequest{
+		Parameter1: pm1,
+		Parameter2: pm2,
+	}
+
+	var res *models.AddResponse
+	if res, err = h.c.Add(reqM); err != nil {
+		return nil, err
+	}
+
+	return &api.CalculatorResponseWithStatus{
+		Message: &api.CalculatorResponseWithStatus_Response{
+			Response: &api.CalculatorResponse{
+				Result: fmt.Sprintf("%f", res.Result),
+			},
+		},
+	}, nil
+}
+
+func (h *GrpcHandler) Add_Old(ctx context.Context, req *api.CalculatorRequest) (*api.CalculatorResponse, error) {
 	var pm1, pm2 float64
 	var err error
 	pm1, err = strconv.ParseFloat(req.Parameter1, 64)
